d799a4c7465b2fe3551affade85436b6bea39828
Manan Gupta <guptamanan1991@gmail.com>
2022-02-25 23:38:26 +0530

    Add filter for cache busting and setup a block tag to inject html in the head tag

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,26 @@
 module.exports = (config) => {
+  config.addWatchTarget("./src/sass");
+  config.addPassthroughCopy("./src/sass");
+  config.addPassthroughCopy("images");
+
+  // Filter for cache busting. Adds a ?v=<UNIX timestamp at build time>
+  // query string to the URL.
+  // use as <img src="{{ '/path/to/file' | url | bust }}" />
+  config.addFilter("bust", (url) => {
+    const [urlPart, paramPart] = url.split("?");
+    const params = new URLSearchParams(paramPart || "");
+    params.set("v", Math.floor(Date.now() / 1000));
+    return `${urlPart}?${params}`;
+  });
+
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dir: {
       input: "src",
-      output: "dist",
+      output: "public",
+      layouts: "_includes/layouts",
     },
   };
 };

79feb5e5c134dfd9f628da1422370d5b5ebbb917
Manan Gupta <guptamanan1991@gmail.com>
2022-03-10 07:58:05 +0530

    Set up fluid typography & spacing. Finish header with responsive navbar.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+const { urlFor } = require("./src/sanity");
+const { inspect } = require("util");
+
 module.exports = (config) => {
   config.addWatchTarget("./src/sass");
   config.addPassthroughCopy("./src/sass");
-  config.addPassthroughCopy("images");
+  config.addPassthroughCopy("./src/assets");
 
   // Filter for cache busting. Adds a ?v=<UNIX timestamp at build time>
   // query string to the URL.
@@ -13,9 +16,23 @@ module.exports = (config) => {
     return `${urlPart}?${params}`;
   });
   // Utility filter to debug JSON objects returned from fetch requests
-  config.addFilter("prettyJSON", (obj) => {
-    return JSON.stringify(obj, null, 2);
+  config.addFilter("inspectJSON", (obj) => {
+    return inspect(obj, { depth: 4 });
   });
+  // Filter to build URLs for Sanity images. Supply with image _id and width.
+  // Usage: {{ _id | urlFor(width) }}
+  config.addFilter("urlFor", (source, width) => {
+    return urlFor(source).width(width).url();
+  });
+  // Filter to console log data
+  config.addFilter("console", (value) => {
+    console.log(inspect(value, { depth: 4 }));
+    return "";
+  });
+  // Filter to word wrap strings
+  // config.addFilter("wordWrap", (s, w) =>
+  //   s.replace(new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, "g"), "$1\n")
+  // );
 
   return {
     markdownTemplateEngine: "njk",
